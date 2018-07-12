196470722003cda53608c391bae5a1606f5245fe
aluoma1 <aluoma1@emich.edu>
2018-07-12 17:39:22 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,7 +1,11 @@
 
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
+  create_olympics_hash = {:Sydney => 2000, :Athens => 2004, :Beijing => 2008, :London => 2012}
+  create_olympics_hash[:Atlanta => 1996]
+  puts create_olympics_hash
 end
+
 
 def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
